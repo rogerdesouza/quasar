8c7a57c1f345889953bdfff9a9ba45d97c19e2de
roger <roger@policiacivil.pe.gov.br>
2021-05-06 15:16:22 -0300

    ajustes

---
+++ components/src/pages/Index.vue
@@ -0,0 +1,52 @@
+<template>
+  <q-page class="flex flex-center">
+    <!-- <q-input
+      label="Name"
+      v-model="name"
+      outlined
+      rounded
+      color="green"
+      standout="bg-green text-white"
+    >
+      <template v-slot:prepend>
+        <q-icon name="person" />
+      </template>
+    </q-input>
+    <q-input
+      label="Name"
+      v-model="name"
+      outlined
+      color="green"
+    >
+    can use append, before or after to
+      <template v-slot:prepend>
+        <q-icon name="person" />
+      </template>
+    </q-input>
+    -->
+    <q-btn
+      color="red"
+      label="Buy Me!!"
+      no-caps
+      push
+      icon="flag"
+      @click="$q.notify({
+        message: 'Crushing it at quasar!',
+        color: 'blue',
+        position: 'center',
+        icon: 'thumb_up'
+      })"
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      name: ''
+    }
+  }
+}
+</script>
---
+++ components/src/pages/QAjaxBarPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page padding>
+    QAjaxBarPage!
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'QAjaxBarPage'
+}
+</script>
